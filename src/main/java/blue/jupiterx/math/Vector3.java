089396af0b1b55631f52e3b5cd5290ff8570fa53
Megapix96 <official.megapixel@gmail.com>
2017-09-30 23:47:07 +0900

    Update Vector (PMMP仕様)

--- src/main/java/blue/jupiterx/math/Vector3.java
+++ src/main/java/blue/jupiterx/math/Vector3.java
@@ -17,4 +17,294 @@
 
 public class Vector3 {
 
+    public static final int SIDE_DOWN = 0;
+    public static final int SIDE_UP = 1;
+    public static final int SIDE_NORTH = 2;
+    public static final int SIDE_SOUTH = 3;
+    public static final int SIDE_WEST = 4;
+    public static final int SIDE_EAST = 5;
+
+    public double x;
+    public double y;
+    public double z;
+
+    public Vector3() {
+        this(0, 0, 0);
+    }
+
+    public Vector3(double x) {
+        this(x, 0, 0);
+    }
+
+    public Vector3(double x, double y) {
+        this(x, y, 0);
+    }
+
+    public Vector3(double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public double getZ() {
+        return z;
+    }
+
+    public int getFloorX() {
+        return (int) Math.floor(x);
+    }
+
+    public int getFloorY() {
+        return (int) Math.floor(y);
+    }
+
+    public int getFloorZ() {
+        return (int) Math.floor(z);
+    }
+
+    public double getRight() {
+        return x;
+    }
+
+    public double getUp() {
+        return y;
+    }
+
+    public double getForward() {
+        return z;
+    }
+
+    public double getSouth() {
+        return x;
+    }
+
+    public double getWest() {
+        return z;
+    }
+
+    public Vector3 add(double x) {
+        return add(x, 0, 0);
+    }
+
+    public Vector3 add(double x, double y) {
+        return add(x, y, 0);
+    }
+
+    public Vector3 add(double x, double y, double z) {
+        return new Vector3(this.x + x, this.y + y, this.z + z);
+    }
+
+    public Vector3 add(Vector3 pos) {
+        return new Vector3(this.x + pos.x, this.y + pos.y, this.z + pos.z);
+    }
+
+    public Vector3 subtract() {
+        return this.subtract(0, 0, 0);
+    }
+
+    public Vector3 subtract(double x) {
+        return this.subtract(x, 0, 0);
+    }
+
+    public Vector3 subtract(double x, double y) {
+        return this.subtract(x, y, 0);
+    }
+
+    public Vector3 subtract(double x, double y, double z) {
+        return this.add(-x, -y, -z);
+    }
+
+    public Vector3 subtract(Vector3 pos) {
+        return this.add(-pos.x, -pos.y, -pos.z);
+    }
+
+    public Vector3 multiply(double number) {
+        return new Vector3(x * number, y * number, z * number);
+    }
+
+    public Vector3 divide(double number) {
+        return new Vector3(x / number, y / number, z / number);
+    }
+
+    public Vector3 ceil() {
+        return new Vector3((int) Math.ceil(x), (int) Math.ceil(y), (int) Math.ceil(z));
+    }
+
+    public Vector3 floor() {
+        return new Vector3((int) Math.floor(x), (int) Math.floor(y), (int) Math.floor(z));
+    }
+
+    public Vector3 round() {
+        return new Vector3(Math.round(this.x), Math.round(this.y), Math.round(this.z));
+    }
+
+    public Vector3 abs() {
+        return new Vector3((int) Math.abs(this.x), (int) Math.abs(this.y), (int) Math.abs(this.z));
+    }
+
+    public Vector3 getSide(int side) {
+        return getSide(side, 1);
+    }
+
+    public Vector3 getSide(int side, int step) {
+        switch (side) {
+            case Vector3.SIDE_DOWN:
+                return new Vector3(this.x, this.y - step, this.z);
+            case Vector3.SIDE_UP:
+                return new Vector3(this.x, this.y + step, this.z);
+            case Vector3.SIDE_NORTH:
+                return new Vector3(this.x, this.y, this.z - step);
+            case Vector3.SIDE_SOUTH:
+                return new Vector3(this.x, this.y, this.z + step);
+            case Vector3.SIDE_WEST:
+                return new Vector3(this.x - step, this.y, this.z);
+            case Vector3.SIDE_EAST:
+                return new Vector3(this.x + step, this.y, this.z);
+            default:
+                return this;
+        }
+    }
+
+    public Vector3 asVector3() {
+        return new Vector3(x, y, z);
+    }
+
+    public static int getOppositeSide(int side){
+        if(side >= 0 && side <= 5){
+            return side ^ 0x01;
+        }
+
+        throw new IllegalArgumentException();
+    }
+
+    public double distance(Vector3 pos) {
+        return Math.sqrt(this.distanceSquared(pos));
+    }
+
+    public double distanceSquared(Vector3 pos) {
+        return Math.pow(this.x - pos.x, 2) + Math.pow(this.y - pos.y, 2) + Math.pow(this.z - pos.z, 2);
+    }
+
+    public double maxPlainDistance() {
+        return this.maxPlainDistance(0, 0);
+    }
+
+    public double maxPlainDistance(double x) {
+        return this.maxPlainDistance(x, 0);
+    }
+
+    public double maxPlainDistance(double x, double z) {
+        return Math.max(Math.abs(this.x - x), Math.abs(this.z - z));
+    }
+
+    public double maxPlainDistance(Vector2 pos) {
+        return this.maxPlainDistance(pos.x, pos.y);
+    }
+
+    public double maxPlainDistance(Vector3 pos) {
+        return this.maxPlainDistance(pos.x, pos.z);
+    }
+
+    public double length() {
+        return Math.sqrt(this.lengthSquared());
+    }
+
+    public double lengthSquared() {
+        return this.x * this.x + this.y * this.y + this.z * this.z;
+    }
+
+    public Vector3 normalize() {
+        double len = this.lengthSquared();
+        if (len > 0) {
+            return this.divide(Math.sqrt(len));
+        }
+        return new Vector3(0, 0, 0);
+    }
+
+    public double dot(Vector3 v) {
+        return this.x * v.x + this.y * v.y + this.z * v.z;
+    }
+
+    public Vector3 cross(Vector3 v) {
+        return new Vector3(
+                this.y * v.z - this.z * v.y,
+                this.z * v.x - this.x * v.z,
+                this.x * v.y - this.y * v.x
+        );
+    }
+
+    public Vector3 getIntermediateWithXValue(Vector3 v, double x) {
+        double xDiff = v.x - this.x;
+        double yDiff = v.y - this.y;
+        double zDiff = v.z - this.z;
+        if (xDiff * xDiff < 0.0000001) {
+            return null;
+        }
+        double f = (x - this.x) / xDiff;
+        if (f < 0 || f > 1) {
+            return null;
+        } else {
+            return new Vector3(this.x + xDiff * f, this.y + yDiff * f, this.z + zDiff * f);
+        }
+    }
+
+    public Vector3 getIntermediateWithYValue(Vector3 v, double y) {
+        double xDiff = v.x - this.x;
+        double yDiff = v.y - this.y;
+        double zDiff = v.z - this.z;
+        if (yDiff * yDiff < 0.0000001) {
+            return null;
+        }
+        double f = (y - this.y) / yDiff;
+        if (f < 0 || f > 1) {
+            return null;
+        } else {
+            return new Vector3(this.x + xDiff * f, this.y + yDiff * f, this.z + zDiff * f);
+        }
+    }
+
+    public Vector3 getIntermediateWithZValue(Vector3 v, double z) {
+        double xDiff = v.x - this.x;
+        double yDiff = v.y - this.y;
+        double zDiff = v.z - this.z;
+        if (zDiff * zDiff < 0.0000001) {
+            return null;
+        }
+        double f = (z - this.z) / zDiff;
+        if (f < 0 || f > 1) {
+            return null;
+        } else {
+            return new Vector3(this.x + xDiff * f, this.y + yDiff * f, this.z + zDiff * f);
+        }
+    }
+
+    public Vector3 setComponents(double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return "Vector3(x=" + this.x + ",y=" + this.y + ",z=" + this.z + ")";
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (!(obj instanceof Vector3)) {
+            return false;
+        }
+
+        Vector3 pos = (Vector3) obj;
+        return x == pos.x && y == pos.y && z == pos.z;
+    }
 }
