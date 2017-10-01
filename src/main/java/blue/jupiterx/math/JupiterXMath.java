6e87ed34dfd0470b472f31088eb3f444ea79abfc
Megapix96 <official.megapixel@gmail.com>
2017-10-01 10:41:28 +0900

    BOM修正

---
+++ src/main/java/blue/jupiterx/math/JupiterXMath.java
@@ -0,0 +1,23 @@
+﻿package blue.jupiterx.math;
+
+/**     _             _ _          __  __
+ *     | |_   _ _ __ (_) |_ ___ _ _\ \/ /
+ *  _  | | | | | '_ \| | __/ _ \ '__\  /
+ * | |_| | |_| | |_) | | ||  __/ |  /  \
+ *  \___/ \__,_| .__/|_|\__\___|_| /_/\_\
+ *             |_|
+ *
+ * These codes are from PocketMine-MP.
+ *
+ * JupiterX Project
+ *
+ * @author JupiterDevelopmentTeam
+ * @link http://jupiterx.blue
+ */
+
+public class JupiterXMath {
+
+    public static Vector2 getDirection2D(double azimuth) {
+        return new Vector2(Math.cos(azimuth), Math.sin(azimuth));
+    }
+}
